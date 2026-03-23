b8ca0e834c3f21b04a5313fb7725a82a2ab836e2
khongkoemtong <kkoemtong@gmail.com>
2026-03-23 13:58:12 +0700

    first push

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+
+      <h1 className='text-9xl font-bold text-blue-300 text-center'>hello world from react js</h1>
+      
+    </div>
+  )
+}
+
+export default App
